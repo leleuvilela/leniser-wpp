08e4f5907a099d69e5dbbd2cc2183d04ae5a2c0e
Vinicius <ext.vinicius.nunes@farfetch.com>
2024-07-04 14:27:08 +0100

    feat: better message filtering

--- src/listeners/messageCreate.ts
+++ src/listeners/messageCreate.ts
@@ -1,6 +1,13 @@
+import WAWebJS from "whatsapp-web.js";
+import { mongoClient } from "../clients/mongo";
 import { wwebClient } from "../clients/wweb";
 import { handlePing, handleBot, handleChecagem, handleFala, handleImagem, handleRanking, handleTranscrever } from "../events";
 import { MessageObserver } from "../observers/message";
+
+const idGrupoLenise = '556285359995-1486844624@g.us'
+const idGrupoLeniseGames = '556299031117-1523720875@g.us'
+const idGrupoTeste = '120363311991674552@g.us';
+const idPedroGilso = '556283282310@c.us';
 
 const observer = new MessageObserver();
 
@@ -12,7 +19,11 @@ observer.addListener("!imagem", handleImagem);
 observer.addListener("!ranking", handleRanking);
 observer.addListener("!transcrever", handleTranscrever)
 
-wwebClient.on("message_create", msg => {
+wwebClient.on("message_create", async msg => {
+
+    if (!shouldProcessMessage(msg)) {
+        return;
+    }
 
     const messageBody = msg.body.toLowerCase();
 
@@ -20,3 +31,13 @@ wwebClient.on("message_create", msg => {
     observer.notify(event, msg)
 
 });
+
+function shouldIgnoreMessage(msg: any): boolean {
+    return (
+        msg.from !== idGrupoLenise &&
+        msg.from !== idGrupoLeniseGames &&
+        msg.from !== idGrupoTeste) || (
+        msg.to !== idGrupoLenise &&
+        msg.to !== idGrupoLeniseGames &&
+        msg.to !== idGrupoTeste)
+}
