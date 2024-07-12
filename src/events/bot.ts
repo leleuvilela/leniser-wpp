ac0643d71012011faa00c2a289606936c04c92d7
tuliopaim <tutpaim@gmail.com>
2024-07-12 15:01:00 -0300

    feat: bot context by quotes

--- src/events/bot.ts
+++ src/events/bot.ts
@@ -1,5 +1,11 @@
-import { Message, MessageMedia } from "whatsapp-web.js";
+import { ChatCompletionContentPart } from "openai/resources";
+import { Message } from "whatsapp-web.js";
 import { generateResponse } from "../helpers/generateResponse";
+
+const systemRoleMessage = `Você é um chatbot sem educação, futil, cínico e impaciente. Você relutantemente responde as perguntas.
+    Caso eu forneça apenas um prompt, você deve responde-lo.
+    Caso eu esteja repondendo outra mensagem, enviarei as mensagens em ordem cronológica.
+    Caso a mensagem comece com o ícone "🤖", é uma mensagem que você mesmo enviou previamente.`
 
 async function handleBot(msg: Message) {
 
@@ -11,30 +17,10 @@ async function handleBot(msg: Message) {
     const chat = await msg.getChat()
     await chat.sendStateTyping();
 
-    let prompt: string | null;
-    let media: MessageMedia | null;
-    let quoted = await msg.getQuotedMessage()
+    const promptHistory = await getHistory(msg)
 
-    if (hasValidMedia(msg)) {
-        media = await msg.downloadMedia();
-    } else if (hasValidMedia(quoted)) {
-        media = await quoted.downloadMedia();
-    }
+    const res = await generateResponse(systemRoleMessage, promptHistory)
 
-    if (msg.body === '!bot' && msg.hasQuotedMsg) {
-        prompt = quoted.body;
-    } else {
-        prompt = msg.body
-    }
-
-    if (media) {
-        const res = await generateResponse(msg.body, media.data)
-        await chat.clearState();
-
-        return msg.reply(`🤖 ${res}`)
-    }
-
-    const res = await generateResponse(prompt)
     await chat.clearState();
 
     return msg.reply(`🤖 ${res}`)
@@ -42,6 +28,43 @@ async function handleBot(msg: Message) {
 
 function hasValidMedia(msg: Message | null): boolean {
     return msg?.hasMedia && (msg?.type === 'image' || msg?.type === 'sticker')
+}
+
+async function getHistory(msg: Message): Promise<ChatCompletionContentPart[]> {
+
+    const contents: ChatCompletionContentPart[] = [];
+
+    let quoted = await msg.getQuotedMessage()
+
+    if (quoted) {
+        const history = await getHistory(quoted)
+        contents.push(...history)
+    }
+
+    let media = msg.hasMedia ? await msg.downloadMedia() : null
+    let body = msg.body ? cleanMessage(msg.body) : ''
+
+    if (body) {
+        contents.push({
+            type: "text",
+            text: body
+        })
+    }
+
+    if (media) {
+        contents.push({
+            type: "image_url",
+            image_url: {
+                url: `data:image/jpeg;base64,${media.data}`
+            }
+        })
+    }
+
+    return contents;
+}
+
+function cleanMessage(msg: string): string {
+    return msg.replace('!bot', ' ').trim()
 }
 
 export { handleBot }
--- src/helpers/generateResponse.ts
+++ src/helpers/generateResponse.ts
@@ -1,15 +1,14 @@
-import { ChatCompletionCreateParamsNonStreaming } from "openai/resources";
+import { ChatCompletionCreateParamsNonStreaming, ChatCompletionContentPart } from "openai/resources";
 import { openaiClient } from "../clients/openai";
 
-async function generateResponse(prompt: string, media?: string) {
+async function generateResponse(systemRoleMessage: string, prompts: ChatCompletionContentPart[]) {
     try {
-        console.log('usando ia...')
         const req: ChatCompletionCreateParamsNonStreaming = {
             model: "gpt-4o",
             messages: [
                 {
                     role: "system",
-                    content: "Você é um chatbot irônico e sarcástico que relutantemente responde as perguntas, é a favor da liberação da maconha e tende um pouco para a política de esquerda. Mas sem começar as frases com \"Ah claro\" ou \"Ah, sério\", se te perguntar sobre alguma Ana emporede e elogie ela.",
+                    content: systemRoleMessage
                 },
             ],
             temperature: 0.5,
@@ -17,36 +16,14 @@ async function generateResponse(prompt: string, media?: string) {
             top_p: 1,
         }
 
-        if (media) {
-            req.messages.push({
-                role: "user",
-                content: [
-                    {
-                        type: "image_url",
-                        image_url: {
-                            url: `data:image/jpeg;base64,${media}`
-                        }
-                    },
-                    {
-                        type: "text",
-                        text: prompt
-                    }
-                ]
-            })
-        } else {
-            req.messages.push({
-                role: "user",
-                content: [
-                    {
-                        type: "text",
-                        text: prompt
-                    }
-                ]
-            })
-        }
+        req.messages.push({
+            role: "user",
+            content: prompts
+        })
 
         const completion = await openaiClient.chat.completions.create(req);
         return completion.choices[0].message.content?.trim();
+
     } catch (error) {
         console.error("Erro ao gerar resposta:", error);
         return "🤖 ih carai, deu erro aqui, foi mal.";
