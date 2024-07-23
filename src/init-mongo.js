caf45e336367999ae632c82831242c4e9fdfc423
tuliopaim <tutpaim@gmail.com>
2024-07-23 00:00:03 -0300

    feat: local mongo with script

--- src/init-mongo.js
+++ src/init-mongo.js
@@ -1,4 +1,11 @@
-/* eslint-disable no-undef */
-
 db = db.getSiblingDB('rap');
 db.createCollection('messages');
+db.createCollection('allowedNumbers');
+
+db.allowedNumbers.insertMany([
+    {
+        _id: '556285359995-1486844624@g.us',
+        desc: 'Grupo de Rap',
+        permissions: ['MESSAGE_CREATE', 'MESSAGE_REVOKE', 'SAVE_MESSAGE']
+    },
+])
