c1316ab763fe0f7d176b983b7efe933c619095a5
tuliopaim <tutpaim@gmail.com>
2024-08-06 10:35:59 -0300

    feat: save message and full message

--- src/application/contracts/IMessagesRepository.ts
+++ src/application/contracts/IMessagesRepository.ts
@@ -1,8 +1,10 @@
 import { MessageCountDto } from '../dtos/messageCountDto';
 import { IMessage } from '../dtos/message';
+import { Message } from 'whatsapp-web.js';
 
 export interface IMessageRepository {
     addMessage: (msg: IMessage) => Promise<boolean>;
+    addFullMessage: (msg: Message) => Promise<boolean>;
     getMessageCountsByUser(
         startDate: Date,
         endDate: Date,
